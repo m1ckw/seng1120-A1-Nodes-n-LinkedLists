e5b2d9f1a63459b87b8f6bd66437d3140ad0d3ca
The-Gary <mick.wiedermann@gmail.com>
2021-04-02 13:49:56 +1100

    Updated Pre and Post Conditions

--- Node.h
+++ Node.h
@@ -2,7 +2,7 @@
 //*********************************************************************************************//
 //  Class Node (header)                                                                        //
 //  Author: Mick Wiedermann                                                                    //
-//  Course: SENG1120                                                                           //
+//  Course: SENG1120 | Assignment 1                                                            //
 //  Date  : 2021-04-01                                                                         //
 //  Program Description: Header file for the Node Object to be used within a linked list.      //
 //***********************************************************************************************
@@ -19,32 +19,37 @@ class Node
         typedef Train dataType;
 
         ////////// CONSTRUCTORS /////////////////////////
+
         // Precondition: None.
-        // Postcondition: 
+        // Postcondition: An empty node is created with the previous and next pointers set to NULL. 
         Node();
-        // Precondition:
-        // Postcondition:
+
+        // Precondition: None. 
+        // Postcondition: A new node is created and initialised with the data passed.
         Node(const dataType& initData, Node* initNext = NULL, Node* initPrevious = NULL);
 		
         ////////// DESTRUCTOR ///////////////////////////
-        // Precondition:
-        // Postcondition:
+
+        // Precondition: A node must exist. 
+        // Postcondition: Deletes the node and sets the next and previous pointers to NULL.
         ~Node();
 
         ////////// MUTATOR FUNCTIONS (SETTERS) //////////
-        // Precondition:
-        // Postcondition:
+
+        // Precondition: A node must exist.
+        // Postcondition: Updates the data within the node to the data passed. 
         void setData(const dataType& newData);  
 
-        // Precondition:
-        // Postcondition:
+        // Precondition: A node must exist. 
+        // Postcondition: Sets the current nodes next pointer to the location passed. 
         void setNext (Node* nextLink);   
 
-        // Precondition:
+        // Precondition: 
         // Postcondition:           
         void setPrevious (Node* previousLink);  
 
         ////////// ACCESSOR FUNCTIONS (GETTERS) /////////
+
         // Precondition:
         // Postcondition:
         const dataType getData() const;
@@ -64,9 +69,9 @@ class Node
         Node* getPrevious();                           
 
     private:
-        dataType data;                      // Private member variable that stores the datatypes data.
-        Node* next;                         // Private member variable that points to the next node.   
-        Node* previous;                     // Private member variable that points to the next node.
+        dataType data;                              // Private member variable that stores the data types data.
+        Node* next;                                 // Private member variable that points to the next node.   
+        Node* previous;                             // Private member variable that points to the next node.
 
 };
 
